e8d0854d8fdf7daa1422c2cce44df88e635b7e0d
Jordan <jconheasman@gmail.com>
2020-11-03 16:51:43 -0800

    issues with z-indexing the stetched link and the event listeners to remove the css stylings

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-var tr = document.querySelector(".trigger");
-var menu = document.querySelector(".menu");
-let isVisible = false;
+// let overlayBtn = document.querySelector('.overlay-btn');
+// let cardBody = document.querySelector('.card-body');
 
-tr.onclick = function () {
-  if (!isVisible) {
-    classie.addClass(menu, "menu--show");
-    classie.removeClass(menu, "menu--hide");
-    isVisible = !isVisible;
-  } else {
-    classie.removeClass(menu, "menu--show");
-    classie.addClass(menu, "menu--hide");
-    isVisible = !isVisible;
-  }
-  // classie.toggle(menu, 'menu--show' );
-};
+let funcTest = document.querySelector('.funcTest')
+let hide = document.querySelector('.hide');
+let show = document.querySelector('.hidden')
+let close = document.querySelector("overlay-close");
+
+// overlayBtn.addEventListener('click', function() {
+//   cardBody.classList.add('bg-active');
+// })
+
+function on() {
+  funcTest.classList.add("bg-active");
+  hide.classList.add("inactive");
+  $(".hidden").removeClass("hidden");
+}
+function off() {
+  funcTest.removeClass("bg-active");
+  // hide.removeClass("inactive");
+  // $(".hidden").classList.add("hidden");
+}
