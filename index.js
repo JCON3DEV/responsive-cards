ddf16ea62c259b7ab8d75ed1e823fde7b58a652d
Jordan <jconheasman@gmail.com>
2020-11-04 12:55:14 -0800

    removed unneccasary comments. still requires obsolete code removed.

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-// let overlayBtn = document.querySelector('.overlay-btn');
-// let cardBody = document.querySelector('.card-body');
-
 let funcTest = document.querySelector('.funcTest')
 let hide = document.querySelector('.hide');
 let show = document.querySelector('.hidden')
 let close = document.querySelector("overlay-close");
 
-// overlayBtn.addEventListener('click', function() {
-//   cardBody.classList.add('bg-active');
-// })
-
 function on() {
   document.getElementById("overlay").style.display = "block";
-  // funcTest.classList.add("bg-active");
-  // hide.classList.add("inactive");
-  // $(".hidden").removeClass("hidden");
 }
 function off() {
   document.getElementById("overlay").style.display = "none";
-  // funcTest.removeClass("bg-active");
-  // hide.removeClass("inactive");
-  // $(".hidden").classList.add("hidden");
 }
 function on2() {
   document.getElementById("overlay-2").style.display = "block";
